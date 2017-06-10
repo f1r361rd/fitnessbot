40372c4ba5d971f77cfa8887c65d3ab27c2ee2b6
f1r361rd <f1r361rd@gmail.com>
2017-06-10 06:43:18 +0530

    fifth

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
 
 const apiController = require('./controllers/apiController');
-
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -15,6 +15,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', verificationController);
 app.post('/', messageWebhookController);
 app.post('/ai',apiController);
-app.listen(8080,()=>{
-    console.log("Listening @ port 8080");
+app.listen(port,()=>{
+    console.log("Listening @ port ");
 });
